06391264cba45a7c07278f3b2f767cd7fa707bbe
= <=>
2025-09-13 23:40:48 +0200

    Initial commit: basic project structure

---
+++ main.py
@@ -0,0 +1,8 @@
+def saluer_le_monde():
+    """Une fonction simple pour vérifier que tout fonctionne."""
+    message = "Hello, le monde de l'automatisation !"
+    print(message)
+    #return message
+
+if __name__ == "__main__":
+    saluer_le_monde()
